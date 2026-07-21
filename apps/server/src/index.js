c04733d2e99e4b012b9ed5eca80a3f6c7368437d
Egorict <egoregor2008@gmail.com>
2026-07-21 19:45:00 +0300

    1

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -8,8 +8,16 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(routes);
 
-const cors = require('cors');
-app.use(cors()); // разрешает все запросы
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // разрешаем все источники (для теста)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  // Если это preflight (OPTIONS), отвечаем сразу
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.get('/health', (req, res) => {
   res.send('OK 1');
